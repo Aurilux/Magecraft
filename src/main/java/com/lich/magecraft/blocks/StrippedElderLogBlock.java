159bce9f18d17fbec96fa53f452c1f40d0bf76db
VidOsep <vid.osep@gmail.com>
2020-07-14 23:01:49 +0200

    Additional fix for stripped elder log

--- src/main/java/com/lich/magecraft/blocks/StrippedElderLogBlock.java
+++ src/main/java/com/lich/magecraft/blocks/StrippedElderLogBlock.java
@@ -1,11 +1,12 @@
 package com.lich.magecraft.blocks;
 
 import net.minecraft.block.Block;
+import net.minecraft.block.RotatedPillarBlock;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.material.MaterialColor;
 
-public class StrippedElderLogBlock extends Block {
+public class StrippedElderLogBlock extends RotatedPillarBlock {
 
     private static Float hardness   = 3.0F;
     private static Float resistance = 6.0F;
