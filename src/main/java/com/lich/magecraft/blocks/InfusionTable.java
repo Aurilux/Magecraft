11296f5e2297a2634757c5f257005ce2c3e37559
VidOsep <vid.osep@gmail.com>
2020-07-19 12:26:50 +0200

    Item texture angle view fix

--- src/main/java/com/lich/magecraft/blocks/InfusionTable.java
+++ src/main/java/com/lich/magecraft/blocks/InfusionTable.java
@@ -2,15 +2,36 @@ package com.lich.magecraft.blocks;
 
 import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.material.MaterialColor;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.shapes.IBooleanFunction;
+import net.minecraft.util.math.shapes.ISelectionContext;
+import net.minecraft.util.math.shapes.VoxelShape;
+import net.minecraft.util.math.shapes.VoxelShapes;
+import net.minecraft.world.IBlockReader;
+
+import java.util.stream.Stream;
 
 public class InfusionTable extends Block {
 
     // tweak this
     private static final Float hardness = 3.0F;
     private static final Float resistance = 3.0F;
+
+
+    private static final VoxelShape SHAPE = Stream.of(
+            Block.makeCuboidShape(0, 0, 0, 16, 4, 16),
+            Block.makeCuboidShape(0, 10, 0, 16, 16, 16),
+            Block.makeCuboidShape(1, 4, 1, 15, 10, 15)
+    ).reduce((v1, v2) -> {return VoxelShapes.combineAndSimplify(v1, v2, IBooleanFunction.OR);}).get();
+
+    @Override
+    public VoxelShape getShape(BlockState state, IBlockReader worldIn, BlockPos pos, ISelectionContext context) {
+        return SHAPE;
+    }
 
     public InfusionTable() {
         super(AbstractBlock.Properties.create(Material.WOOD)
